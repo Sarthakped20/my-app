40b0b2217b2b81e9a4ec5de6e6df68fa124c287c
Sarthakped20 <139477798+Sarthakped20@users.noreply.github.com>
2024-07-05 20:22:41 +0530

    states and hooks

--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React , {useState} from "react";
 
 export default function Textbox(props) {
+  const handleUpClick = ()=>{
+    console.log("clicked "+text);
+    const newText = text.toUpperCase();
+    SetText(newText);
+
+  }
+  const handleOnChange = (event)=>{
+    console.log("Change");
+    SetText(event.target.value)
+
+  }
+  const handleLoClick = (event)=>{
+    console.log("LoClicked "+text);
+    const newText = text.toLowerCase();
+    SetText(newText);
+
+  }
+
+  const [text,SetText]= useState("");
+  // SetText = "sarthak"
+  // console.log(useState("Enter text here"))
   return (
     <div>
         <div>
@@ -14,9 +35,12 @@ export default function Textbox(props) {
           className="form-control"
           id="exampleFormControlTextarea1"
           rows="10"
+          value={text}
+          onChange={handleOnChange}
         ></textarea>
 
-        <button className="btn btn-primmary my-3">Convert to upperCase</button>
+        <button className="btn btn-primmary my-3" onClick={handleUpClick} >Convert to upperCase</button>
+        <button className="btn btn-primary2 my3"onClick={handleLoClick}>Convert to lowerCase</button>
       </div>
     </div>
   );
